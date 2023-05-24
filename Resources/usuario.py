48e1babc2a6c122222dbcf426dab80da46bb9944
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-05-23 22:37:12 -0300

    hash token

--- Resources/usuario.py
+++ Resources/usuario.py
@@ -1,6 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from Api_hotel.app import bcrypt
+
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True,
+                       help="te field 'login' cannot be left blanc")
+atributos.add_argument('senha', type=str, required=True,
+                       help="te field 'senha' cannot be left blanc")
 
 
 class User(Resource):
@@ -23,17 +32,29 @@ class User(Resource):
             return {'message': 'user deleted'}
         return {'message': 'User not found'}
 
+
 class UserRegister(Resource):
     # cadastro
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="te field 'login' cannot be left blanc")
-        atributos.add_argument('senha', type=str, required=True, help="te field 'senha' cannot be left blanc")
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
-            return {"message":" The login '{}'already exist".format(dados['login'])}
-        
+            return {"message": " The login '{}'already exist".format(dados['login'])}
+
         user = UserModel(**dados)
         user.save_user()
-        return {"message": "User created successfully"}, 201 # created
+        return {"message": "User created successfully"}, 201  # created
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+        if user and bcrypt.check_password_hash(dados['senha'], user.senha):
+            token_de_acesso = create_access_token(identify=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        # Unauthorized
+        return {'message': 'The usermane or password is incorrect'}, 401
