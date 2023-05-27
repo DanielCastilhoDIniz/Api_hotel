95dbeb83a912484ade37036d931952b155e1e7a1
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-05-27 00:09:11 -0300

    parametro para query

--- Resources/hotel.py
+++ Resources/hotel.py
@@ -2,13 +2,55 @@ from flask import Flask
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+
+
+def normalize_path_params(cidade=None,
+                          estrelas_min=0,
+                          estrelas_max=5,
+                          diaria_min=0,
+                          diaria_max=100000,
+                          limit=50,
+                          offset=0, **dados):
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'cidade': cidade,
+            'limit': limit,
+            'offset': offset
+        }
+    return {
+        'estrelas_min': estrelas_min,
+        'estrelas_max': estrelas_max,
+        'diaria_min': diaria_min,
+        'diaria_max': diaria_max,
+        'limit': limit,
+        'offset': offset
+    }
+
+
+# path /hoteis?cidade=Rio de Janeiro&estrelas_min=4&diaria_max=400
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estrelas_min', type=float)
+path_params.add_argument('estrelas_max', type=float)
+path_params.add_argument('diaria_min', type=float)
+path_params.add_argument('diaria_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
 
 
 # Define uma classe Hoteis que herda da classe Resource
 class Hoteis(Resource):
-
     # Método GET para a classe Hoteis
+
     def get(self):
+        dados = path_params.parse_args()
+        dados_validos = {chave: dados['chave']
+                         for chave in dados if dados['chave'] is not None}
         return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
 
 
@@ -44,6 +86,7 @@ class Hotel(Resource):
             # Server error
             return {'message': 'An internal error ocurred trying save hotel'}, 500
         return hotel.json()
+
     @jwt_required()
     def put(self, hotel_id):
         dados = Hotel.argumento.parse_args()
@@ -60,6 +103,7 @@ class Hotel(Resource):
             # Server error
             return {'message': 'An internal error ocurred trying save hotel'}, 500
         return hotel.json(), 201  # created
+
     @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
