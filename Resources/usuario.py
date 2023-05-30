25bfa349e178c38ca763fe75f9e693cf8fb8a36d
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-05-29 22:23:28 -0300

    rotas para confirmação de cadastro e validação por email

--- Resources/usuario.py
+++ Resources/usuario.py
@@ -4,6 +4,7 @@ from models.usuario import UserModel
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from passlib.hash import pbkdf2_sha256, bcrypt
 from blacklist import BLACKLIST
+import traceback
 
 
 import jwt
@@ -14,6 +15,7 @@ atributos.add_argument('login', type=str, required=True,
                        help="te field 'login' cannot be left blanc")
 atributos.add_argument('senha', type=str, required=True,
                        help="te field 'senha' cannot be left blanc")
+atributos.add_argument('email', type=str)
 atributos.add_argument('ativado', type=bool)
 
 
@@ -43,14 +45,24 @@ class UserRegister(Resource):
     # cadastro
     def post(self):
         dados = atributos.parse_args()
+        if not dados.get('email') or dados.get('email' is None):
+            return {"message":"The field 'email' cannot be left blank."}, 400
+        
+        if UserModel.find_by_email(dados['email']):
+            return {"message":"The '{}' already exists".format(dados['email'])}, 400
 
         if UserModel.find_by_login(dados['login']):
-
             return {"message": " The login '{}'already exist".format(dados['login'])}, 400 # Bad request
 
         user = UserModel(**dados)
         user.ativado = False
-        user.save_user()
+        try:
+            user.save_user()
+            user.send_confirmation_email()
+        except:
+            user.delete_user()
+            traceback.print_exc()
+            return {"message":"An internal server error has ocurred."}, 500
         return {"message": "User created successfully"}, 201  # created
 
 
--- models/usuario.py
+++ models/usuario.py
@@ -1,24 +1,48 @@
 from sql_alchemy import banco
-import hashlib
+from flask import request, url_for
+from requests import post
 
+
+MAILGUN_DOMAIN ="sandbox42628f61c92542d2a300cf07771a270f.mailgun.org"
+MAILGUN_API_KEY = "3305d42da93d1e1a0fd655998b89a353-5d9bd83c-2024718a"
+FROM_TITLE ='Confirmação'
+FROM_EMAIL = 'no-replay@restapihotel.com'
 
 class UserModel(banco.Model):
     __tablename__ = 'usuarios'
 
     user_id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.String(40))
-    senha = banco.Column(banco.String(40))
+    login = banco.Column(banco.String(40),nullable=False, unique=True)
+    senha = banco.Column(banco.String(40), nullable=False)
+    email = banco.Column(banco.String(90), nullable=False, unique=True)
     ativado = banco.Column(banco.Boolean, default=False  )
 
-    def __init__(self, login, senha, ativado):
+    def __init__(self, login, senha, email, ativado):
         self.login = login
         self.senha = senha
         self.ativado = ativado
 
+    
+    def send_confirmation_email(self):
+        # http://127.0.0.1:5000/confirmacao/{user_id}'
+        link = request.url_root[:-1] + url_for('userconfirm', user_id=self.user_id)
+
+        return post('https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN), 
+                    auth=("api", MAILGUN_API_KEY),
+                    data={"from": "{} <{}>".format(FROM_TITLE, FROM_EMAIL),
+                          "to": self.email,
+                          "subject": "Confirmação de cadastro",
+                          "text": "Confirme seu cadastro clicando no link a seguir: {} ".format(link),
+                          "html":"<html><p>Confirme seu cadastro clicando no link a seguir:<a href='{}'> CONFIRMAR EMAIL</a></p></html>".format(link)
+                          }
+                    )
+          
+
     def json(self):
         return {
             "user_id": self.user_id,
-            "login": self.login,
+            "login": self.login,    
+            "email": self.email,    
             "ativado": self.ativado
         }
 
@@ -34,6 +58,14 @@ class UserModel(banco.Model):
     def find_by_login(cls, login):
         # SELECT * FROM hoteis WHERE login = $login
         user = cls.query.filter_by(login=login).first()
+        if user:
+            return user
+        return None
+    
+    @classmethod
+    def find_by_email(cls, email):
+        # SELECT * FROM hoteis WHERE email = $email
+        user = cls.query.filter_by(email=email).first()
         if user:
             return user
         return None
