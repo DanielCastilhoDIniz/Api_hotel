a087796b4edeef3f2ee51dac3db625d7e0523008
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-05-30 21:46:54 -0300

    ajuste de segurança

--- models/usuario.py
+++ models/usuario.py
@@ -1,10 +1,10 @@
 from sql_alchemy import banco
 from flask import request, url_for
 from requests import post
+from models.keys import MAILGUN_API_KEY
 
 
-MAILGUN_DOMAIN ="sandbox42628f61c92542d2a300cf07771a270f.mailgun.org"
-MAILGUN_API_KEY = "3305d42da93d1e1a0fd655998b89a353-5d9bd83c-2024718a"
+MAILGUN_DOMAIN ="3305d42da93d1e1a0fd655998b89a353-5d9bd83c-2024718a"
 FROM_TITLE ='Confirmação'
 FROM_EMAIL = 'no-replay@restapihotel.com'
 
@@ -12,14 +12,15 @@ class UserModel(banco.Model):
     __tablename__ = 'usuarios'
 
     user_id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.String(40),nullable=False, unique=True)
+    login = banco.Column(banco.String(40), nullable=False, unique=True)
     senha = banco.Column(banco.String(40), nullable=False)
-    email = banco.Column(banco.String(90), nullable=False, unique=True)
-    ativado = banco.Column(banco.Boolean, default=False  )
+    email = banco.Column(banco.String(80), nullable=False, unique=True)
+    ativado = banco.Column(banco.Boolean, default=False)
 
     def __init__(self, login, senha, email, ativado):
         self.login = login
         self.senha = senha
+        self.email =email
         self.ativado = ativado
 
     
